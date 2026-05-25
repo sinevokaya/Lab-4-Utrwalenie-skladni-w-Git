727584072e6d97d4cfb33e749190ae7fedd11a57
Katarzyna <dsw63244@student.dsw.edu.pl>
2026-05-25 02:51:07 +0200

    Dodanie flagi --logs

--- skrypt.sh
+++ skrypt.sh
@@ -2,3 +2,11 @@
 if [ "$1" == "--date" ]; then
     date
 fi
+
+if [ "$1" == "--logs" ]; then
+    for i in $(seq 1 100); do
+        echo "Nazwa pliku: log$i.txt" >log$i.txt
+        echo "Nazwa skryptu: skrypt.sh" >>log$i.txt
+        echo "Data: $(date)" >>log$i.txt
+    done
+fi
