521337f2bc9d0fefb38b1e2a49d010b1b11da545
Katarzyna <dsw63244@student.dsw.edu.pl>
2026-05-25 03:17:29 +0200

    Dodanie flagi --logs z argumentem

--- skrypt.sh
+++ skrypt.sh
@@ -1,12 +1,21 @@
+
 #!/bin/bash
 if [ "$1" == "--date" ]; then
     date
 fi
 
-if [ "$1" == "--logs" ]; then
+if [ "$1" == "--logs" ] && [ -z "$2" ]; then
     for i in $(seq 1 100); do
         echo "Nazwa pliku: log$i.txt" >log$i.txt
         echo "Nazwa skryptu: skrypt.sh" >>log$i.txt
         echo "Data: $(date)" >>log$i.txt
+    done
+fi
+
+if [ "$1" == "--logs" ] && [ -n "$2" ]; then
+    for i in $(seq 1 $2); do
+        echo "Nazwa pliku: log$i.txt" > log$i.txt
+        echo "Nazwa skryptu: skrypt.sh" >> log$i.txt
+        echo "Data: $(date)" >> log$i.txt
     done
 fi
